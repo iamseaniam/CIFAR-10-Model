70a22be1a12819f14a37b76c5627a5bceb59e18c
iamseaniam <7222@holbertonstudents.com>
2024-09-26 13:29:12 -0500

    added files, readme, and model.py

---
+++ Test_Images/Model.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+# tensorboard?
+# AutoMl?
+# AutoKeras?
+# Sklearn random/ other thingy
+# building a model
